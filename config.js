464a86e48e3a816fee915156240ac198c37d61e4
benjaminjenkins1 <benjamin.jenkins1@marist.edu>
2019-01-11 16:48:40 +0000

    sending mail with succcess and failure redirects

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
 const config = {
-  HOST: process.env.APP_HOST,
-  PORT: process.env.APP_PORT,
+  TO_ADDRESS: process.env.TO_ADDRESS,
+  FROM_ADDRESS: process.env.FROM_ADDRESS,
+  APP_HOST: process.env.APP_HOST,
+  APP_PORT: process.env.APP_PORT,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
 };
 
--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 const config = require('./config');
 const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(config.SENDGRID_API_KEY);
 
-app.get('/', function (req, res) {
-  res.send('hello world');
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.post('/', function (req, res) {
+  const msgText = 'From: ' + req.body.name + ':<br>' + req.body.text;
+  const msg = {
+    to: config.TO_ADDRESS,
+    from: config.FROM_ADDRESS,
+    subject: req.body.subject,
+    text: msgText
+  };
+  if(req.body.name && req.body.subject && req.body.text) {
+    sgMail.send(msg);
+    res.redirect(req.body.success_redirect);
+    console.log("Sent a message.");
+    console.log("Redirected to " + req.body.success_redirect);
+  }
+  else {
+    res.redirect(req.body.failure_redirect);
+    console.log("Failed to send a message.");
+    console.log("Redirected to " + req.body.failure_redirect);
+  }
 });
 
-app.listen(config.PORT, config.HOST, () => console.log(`Example app listening at ${config.HOST}:${config.PORT}...`));
+app.listen(config.APP_PORT, config.APP_HOST, () => {
+  console.log(`Mailman listening at ${config.APP_HOST}:${config.APP_PORT}...`);
+});
